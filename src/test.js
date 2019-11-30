1b578365d23813877ee6222289c1708abc646290
felipetestaaa <felipe.testaa@hotmail.com>
2019-11-30 18:47:33 -0300

    Script Update

--- src/test.js
+++ src/test.js
@@ -1,13 +1,17 @@
-"use strict";
-exports.__esModule = true;
 var fs = require("fs");
 var demofile = require("demofile");
+
 fs.readFile("../demos/mirage.dem", function (err, buffer) {
+    
     var demoFile = new demofile.DemoFile();
+
     demoFile.on("start", function () {
-        console.log("Demo header:", demoFile.entities.players);
-        // Stop parsing - we're finished
+        console.log("Tick rate:", demoFile.tickRate);
         demoFile.cancel();
+        return {
+            tickrate: demoFile.tickRate
+        };
     });
+
     demoFile.parse(buffer);
 });
--- src/test.ts
+++ src/test.ts
@@ -1,14 +1,16 @@
+
 import fs = require("fs");
 import demofile = require("demofile");
 
 fs.readFile("../demos/mirage.dem", (err, buffer) => {
   const demoFile = new demofile.DemoFile();
 
-  demoFile.stringTables.on("update", e => {
-    if (e.table.name === "userinfo" && e.userData != null) {
-      console.log("\nPlayer info updated:");
-      console.log(e.entryIndex, e.userData);
-    }
+  demoFile.on("start", () => {
+    console.log("Demo header:", demoFile.header);
+    console.log("Tick rate:", demoFile.tickRate);
+
+    // Stop parsing - we're finished
+    demoFile.cancel();
   });
 
   demoFile.parse(buffer);
