dae86c402ac1190605ad2627f4cfd0b62ecbd991
felipetestaaa <felipe.testaa@hotmail.com>
2019-12-01 13:54:22 -0300

    New Folde Structure Updated

---
+++ server/main.js
@@ -0,0 +1,18 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+var demofile = require("demofile");
+fs.readFile("../demos/mirage.dem", function (_err, buffer) {
+    var demoFile = new demofile.DemoFile();
+    var util = require('util');
+    demoFile.gameEvents.on("player_death", function (e) {
+        var victim = demoFile.entities.getByUserId(e.userid);
+        var attacker = demoFile.entities.getByUserId(e.attacker);
+        var assister = demoFile.entities.getByUserId(e.assister);
+        var victimID = victim ? victim.steam64Id : null;
+        var attackerID = attacker ? attacker.steam64Id : null;
+        var assisterID = assister ? assister.steam64Id : null;
+        console.log(victimID + ", " + attackerID + " , " + assisterID);
+    });
+    demoFile.parse(buffer);
+});
---
+++ server/util/getSteamID.js
@@ -0,0 +1,24 @@
+import fs = require("fs");
+import demofile = require("demofile");
+
+
+fs.readFile("../demos/mirage.dem", (_err, buffer) => {
+  const demoFile = new demofile.DemoFile();
+  const util = require('util');
+
+  demoFile.gameEvents.on("player_death", e => {
+    
+    const victim = demoFile.entities.getByUserId(e.userid);
+    const attacker = demoFile.entities.getByUserId(e.attacker);
+    const assister = demoFile.entities.getByUserId(e.assister);
+    
+
+    const victimID = victim ? victim.steam64Id : null;
+    const attackerID = attacker ? attacker.steam64Id : null;
+    const assisterID = assister ? assister.steam64Id : null;
+
+    console.log(`${victimID}, ${attackerID} , ${assisterID}`);
+  });
+
+  demoFile.parse(buffer);
+});
