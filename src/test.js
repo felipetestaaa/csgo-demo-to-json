ea34cc79417d5c0b5f052398f316e3d7ca1bd32d
felipetestaaa <felipe.testaa@hotmail.com>
2019-12-04 22:53:35 -0300

    Script Update

--- src/test.js
+++ src/test.js
@@ -5,13 +5,28 @@ var demofile = require("demofile");
 fs.readFile("../demos/mirage.dem", function (_err, buffer) {
     var demoFile = new demofile.DemoFile();
     demoFile.gameEvents.on("player_death", function (e) {
-        var victim = demoFile.entities.getByUserId(e.userid);
+        //const defuserPickup = demoFile.entities.getByUserId(e.userid);
+        //const bombsite = demoFile.entities.getByUserId(e.site);
         var attacker = demoFile.entities.getByUserId(e.attacker);
-        var assister = demoFile.entities.getByUserId(e.assister);
-        var victimID = victim ? victim.steam64Id : null;
+        //const defuserPickupID = defuserPickup ? defuserPickup : null;
+        //const bombsiteID = bombsite ? bombsite: null;
         var attackerID = attacker ? attacker.steam64Id : null;
-        var assisterID = assister ? assister.steam64Id : null;
-        console.log(victimID + ", " + attackerID + " , " + assisterID);
+        var attackerName = attacker ? attacker.name : null;
+        var attackerPlaceName = attacker ? attacker.placeName : null;
+        var attackerWeapon = attacker ? attacker.weapon.itemName : null;
+        //console.log(`${victimID}, ${attackerID} , ${assisterID}`);
+        console.log(attackerID + ", " + attackerName + ", " + attackerPlaceName + " , " + attackerWeapon);
+    });
+    demoFile.gameEvents.on("round_end", function (e) {
+        console.log("*** Round ended '%s' (reason: %s, time: %d seconds)", demoFile.gameRules.phase, e.reason, demoFile.currentTime);
+        // We can't print the team scores here as they haven't been updated yet.
+        // See round_officially_ended below.
+    });
+    demoFile.gameEvents.on("round_officially_ended", function (e) {
+        var teams = demoFile.teams;
+        var terrorists = teams[2];
+        var cts = teams[3];
+        console.log("\tTerrorists: %s score %d\n\tCTs: %s score %d", terrorists.clanName, terrorists.score, cts.clanName, cts.score);
     });
     demoFile.parse(buffer);
 });
--- src/test.ts
+++ src/test.ts
@@ -7,16 +7,47 @@ fs.readFile("../demos/mirage.dem", (_err, buffer) => {
 
   demoFile.gameEvents.on("player_death", e => {
     
-    const victim = demoFile.entities.getByUserId(e.userid);
+    //const defuserPickup = demoFile.entities.getByUserId(e.userid);
+    //const bombsite = demoFile.entities.getByUserId(e.site);
     const attacker = demoFile.entities.getByUserId(e.attacker);
-    const assister = demoFile.entities.getByUserId(e.assister);
     
 
-    const victimID = victim ? victim.steam64Id : null;
+    //const defuserPickupID = defuserPickup ? defuserPickup : null;
+    //const bombsiteID = bombsite ? bombsite: null;
     const attackerID = attacker ? attacker.steam64Id : null;
-    const assisterID = assister ? assister.steam64Id : null;
+    const attackerName = attacker ? attacker.name : null;
+    const attackerPlaceName = attacker ? attacker.placeName : null;
+    const attackerWeapon = attacker ? attacker.weapon.itemName : null;
 
-    console.log(`${victimID}, ${attackerID} , ${assisterID}`);
+    //console.log(`${victimID}, ${attackerID} , ${assisterID}`);
+    console.log(`${attackerID}, ${attackerName}, ${attackerPlaceName} , ${attackerWeapon}`);
+  });
+
+  demoFile.gameEvents.on("round_end", e => {
+    console.log(
+      "*** Round ended '%s' (reason: %s, time: %d seconds)",
+      demoFile.gameRules.phase,
+      e.reason,
+      demoFile.currentTime
+    );
+
+    // We can't print the team scores here as they haven't been updated yet.
+    // See round_officially_ended below.
+  });
+
+  demoFile.gameEvents.on("round_officially_ended", e => {
+    const teams = demoFile.teams;
+
+    const terrorists = teams[2];
+    const cts = teams[3];
+
+    console.log(
+      "\tTerrorists: %s score %d\n\tCTs: %s score %d",
+      terrorists.clanName,
+      terrorists.score,
+      cts.clanName,
+      cts.score
+    );
   });
   
   demoFile.parse(buffer);
